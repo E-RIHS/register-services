127421f36704b0c4afd7e2d604cd6a1aead19b75
kobbejager <wfremout+github@gmail.com>
2024-05-14 19:56:29 +0200

    add some documentation

---
+++ src/components/AboutPanel.vue
@@ -0,0 +1,50 @@
+<script setup>
+import 'primeicons/primeicons.css'
+</script>
+
+<template>
+
+        <h2 class="py-6 text-4xl font-bold">
+            Registering Services in the E-RIHS KnowledgeBase
+        </h2>
+
+        <h3 class="py-6 text-xl font-bold">
+            About the E-RIHS KnowledgeBase
+        </h3>
+
+        <p class="mb-4">
+            The E-RIHS Knowledge Base (<a href="https://data.e-rihs.io" class="underline" target="_blank">data.e-rihs.io<span class="pi pi-external-link ml-1 mr-1" /></a>) is a repository of information about the E-RIHS infrastructure, services, and resources. 
+            It is designed to support the E-RIHS community in sharing and discovering information about the E-RIHS infrastructure, services, and resources.
+        </p>
+
+        <p class="mb-4">
+            The Knowledge Base is powered by Cordra, a platform for building and managing digital objects. Cordra is designed to support the creation, 
+            management, and sharing of digital objects, and to provide a flexible and extensible platform for building digital repositories.
+        </p>
+
+        <h3 class="py-6 text-xl font-bold">
+            About the Register-Services tool
+        </h3>
+
+        <p class="mb-4">
+            The Knowledge Base, empowered by Cordra, provides a very basic user interface for creating, managing, and sharing digital objects. Although extremely powerful, it is primarily intended as an administrative user interface.
+            Several user-friendly tools for end users are expected to be developed in the future, which will make underlying use of this Knowledgebase. 
+        </p>
+
+        <p class="mb-4">
+            The Register-Services tool is intended to facilitate the process of registering services and all related documentation within the Knowledge Base. 
+            It guides the service providers through the different steps of the process with a user-friendly interface, and polishes off Cordra's roughest edges.
+            It is not designed to be a full-fledged service management tool.
+        </p>
+
+        <h3 class="py-6 text-xl font-bold">
+            Defining a service
+        </h3>
+
+        <p class="mb-4">
+            In addition to the key metadata directly describing an actual Service, the full definition of an E-RIHS Service is a combination of the individual definitions of a set of related entities (People, Tools, Techniques, etc), 
+            see model diagram, that all need to be defined first in a specific order. 
+        </p>
+        <img src="https://data.e-rihs.io/objects/design?payload=defining_services.png" alt="Schematic view of all definitions around the concept 'Service'" class="mb-4">
+
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,10 @@
 <script setup>
-import StepperNavigation from '@/components/StepperNavigation.vue'
 import { ref, watch } from "vue"
-
 import { useAuthStore } from '@/stores/AuthStore'
 
+
 const CORDRA_BASE_URL = "https://data.e-rihs.io"
+
 
 const items = ref([
     {
@@ -17,7 +17,7 @@ const items = ref([
         label: 'Organisations'
     },
     {
-        label: 'People'
+        label: 'Persons'
     },
     {
         label: 'Equipment'
@@ -53,7 +53,9 @@ const auth = useAuthStore()
             </template>
         </Card>
 
-        <Organisations v-if="activeStep == 2 && auth.accessToken"/>
+        <AboutPanel v-if="activeStep == 0"/>
+        <RegistrationPanel v-else-if="activeStep == 1"/>
+        <OrganisationPanel v-else-if="activeStep == 2 && auth.accessToken"/>
 
         <div v-else>
             You are not authenticated. Please authenticate to proceed.
