8184519489ed1efae25272303becd8d702545d40
kobbejager <wfremout+github@gmail.com>
2024-05-15 22:33:09 +0200

    add google forms in dialog

--- src/components/RegistrationPanel.vue
+++ src/components/RegistrationPanel.vue
@@ -1,9 +1,15 @@
 <script setup>
 
+import { ref } from "vue"
+
 import { useAuthStore } from '@/stores/AuthStore'
 
 // subscribe to auth store to see if a user is logged in (and thus has already registered)
 const auth = useAuthStore()
+
+// toggle visibility of the registration form
+const showRegistrationForm = ref(false)
+const googleFormsUrl = import.meta.env.VITE_GOOGLE_FORMS_URL
 
 </script>
 
@@ -26,12 +32,30 @@ const auth = useAuthStore()
     </p>
     <p class="mb-4">
         However, users need to be pre-registered in the system before they can login; having an ORCID iD is not sufficient.
-        <strong>Registration is done by contacting wim[dot]fremout[at]kikirpa[dot]be</strong>, specifying your ORCID iD, primary e-mail address and details of your primary affiliation/organisation.
-        Organisation details should include a full name and an acronym.
+        <strong>You can request an account using the button below, which will open a form (Google Forms).</strong> 
+        Alternatively, you can also request an account by e-mail (wim[dot]fremout[at]kikirpa[dot]be), specifying your first and last name, 
+        ORCID iD, primary e-mail address and details of your primary affiliation/organisation (full name, acronym, country of operation).
     </p>
     <p class="mb-4">
         A user account will be set up for you, along with details of you as a person and your related organisation. 
-        You will be notified by email when this is done, and you will then be able to login using your ORCID iD.
+        <strong>You will be notified by e-mail when this is done</strong>. From that moment you be able to login using your ORCID iD.
+    </p>
+
+    <p v-if="!auth.userId" class="mb-4">
+        <Button 
+            label="Request account" 
+            icon="pi pi-user-plus"
+            @click="showRegistrationForm = true" 
+        />
+
+        <Dialog 
+            v-model:visible="showRegistrationForm" 
+            modal 
+            header="Registration form" 
+            :style="{ width: '685px' }"
+        >
+            <iframe :src="googleFormsUrl" width="640" height="797" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
+        </Dialog>
     </p>
 
 </template>
